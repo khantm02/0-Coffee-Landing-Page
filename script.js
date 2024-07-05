86c8a227a4a083e509408e23e16534dcc31131ca
Khant Maung <maungkhantdelica@gmail.com>
2024-07-05 18:25:58 +0200

    Starbucks Landing Page

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function switchImg(img) {
+  document.querySelector(".productImg").src = img;
+}
+
+// function changeBackground(color) {
+//   const backgroundColor = document.querySelector("body");
+//   backgroundColor.style.background = color;
+// }
